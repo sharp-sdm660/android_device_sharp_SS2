0b2c499779344a37ff048b5b306749b1feaf5a36
luk1337 <priv.luk@gmail.com>
2019-07-18 19:41:39 +0200

    SS2: Move fingerprint to front
    Change-Id: I3cfc4d198c3a88724e3a268a4e9e9b7ab9909f9b

--- lineage_SS2.mk
+++ lineage_SS2.mk
@@ -35,7 +35,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
