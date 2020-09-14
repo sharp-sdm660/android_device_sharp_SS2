93691572dd1d8a1514f11d17f56b3baee545974b
sjllls <2012ji123@gmail.com>
2020-09-14 14:46:51 +0800

    Initialize ss2

---
+++ device.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Set Shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+
+# Inherit from sharp sdm660-common
+$(call inherit-product, device/sharp/sdm660-common/sdm660.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/sharp/ss2/ss2-vendor.mk)
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.ss2.target.rc
---
+++ lineage_ss2.mk
@@ -0,0 +1,64 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit some common AOSP stuff
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from ss2 device
+$(call inherit-product, device/sharp/ss2/device.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/sharp/ss2/ss2-vendor.mk)
+
+# Inherit from sharp sdm660-common
+$(call inherit-product, device/sharp/sdm660-common/sdm660.mk)
+
+# Set Shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Asserts
+TARGET_OTA_ASSERT_DEVICE := ss2
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 2040
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_NAME := lineage_ss2
+PRODUCT_DEVICE := ss2
+PRODUCT_MANUFACTURER := Sharp
+PRODUCT_BRAND := sharp
+PRODUCT_MODEL := sharp S2
+
+PRODUCT_GMS_CLIENTID_BASE := android-hmd
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=ss2 \
+    PRODUCT_NAME=ss2 \
+    PRIVATE_BUILD_DESC="SAT_00CN_MCDA SAT:8.0.0 OPR1.170623.027 00CN_2_180 release-keys"
+
+BUILD_FINGERPRINT := FIH/SAT_00CN_MCDA/SAT:8.0.0/OPR1.170623.027/00CN_2_180:user/release-keys
