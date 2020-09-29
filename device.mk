eaaa32677c7cfba218ffe1d194fd278053e3af44
sjllls <2012ji123@gmail.com>
2020-09-29 09:15:41 +0800

    SS2: Import nfc configs from stock
    Change-Id: Ia240deacd9e6b9cf1593dcdc544a7b15f2f2dbf2

--- device.mk
+++ device.mk
@@ -28,6 +28,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
+# NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.SS2.target.rc
